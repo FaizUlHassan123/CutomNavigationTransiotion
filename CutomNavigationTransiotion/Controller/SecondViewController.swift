742da4800d77a265ce879ca14d49956ea79f36eb
Faiz Ul Hassan <faizhassan550@gmail.com>
2022-06-25 18:27:21 +0500

    initial commit

---
+++ CutomNavigationTransiotion/Controller/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  CutomNavigationTransiotion
+//
+//  Created by Faiz Ul Hassan on 6/25/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
